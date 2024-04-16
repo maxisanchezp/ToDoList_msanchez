9b36f3d63b7ce028af9ddb443ab9d347fb495c41
maxisanchezp <maxisanchez2001py@gmail.com>
2024-04-15 21:46:53 -0400

    todoapp 15/04

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ToDoList from "./ToDoList.jsx"
+
 import './App.css'
 
 function App () {
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,63 +1,81 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import trashIcon from "./resources/icons8-trash-64.png";
 
-function ToDoList(){
+function ToDoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [hoverIndex, setHoverIndex] = useState();
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
 
-    const [tasks, setTasks] = useState(['Comer algo', 'Bañarse']);
-    const [newTask, setNewTask] = useState("");
-
-    function handleInputChange(event) {
-        setNewTask(event.target.value)
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
     }
+  }
 
-    function addTask() {
-        if (newTask.trim() !== "") {
-            setTasks(t => [...t,newTask]);
-            setNewTask(''); 
-        }
-        
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((element, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function deleteAll() {
+    setTasks([]);
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      addTask();
     }
+  }
 
-    function deleteTask(index){
-        
-    }
-
-    function moveTaskUp(index){
-        
-    }
-    
-    function moveTaskDown(index){
-
-    }
-
-    return(
-        <div className='to-do-list'>
-
-            <h1>Lista de actividades</h1>
-            <div>
-                <input type="text" placeholder='Ingrese la actividad...' value={newTask}
-                onChange={handleInputChange}/>
-                <button className='add-button' onClick={addTask}>Añadir</button>
-            </div>
-
-            <ol>
-                {tasks.map((task, index) => 
-                <li key={index}>
-                    <span className='text'>{task}</span>
-                    <button className='delete-button' onClick={() =>
-                        deleteTask(index)}>Borrar</button>
-                    
-                    <button className='move-button' onClick={() =>
-                        moveTaskUp(index)}>☝️</button>
-                    
-                    <button className='move-button' onClick={() =>
-                        moveTaskUp(index)}>👇</button>
-                </li>
-            )}
-            </ol>
-
+  return (
+    <div className="container">
+      <div className="to-do-list">
+        <h1>ToDo App</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Ingrese la actividad..."
+            value={newTask}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          />
+          <button className="add-button" onClick={addTask}>
+            +
+          </button>
         </div>
-    )
 
+        <ol>
+          {tasks.map((task, index) => (
+            <li
+              key={index}
+              onMouseEnter={() => setHoverIndex(index)}
+              onMouseLeave={() => setHoverIndex()}
+            >
+              <span className="text">{task}</span>
+              {hoverIndex === index && (
+                <button
+                  className="trash-delete"
+                  onClick={() => deleteTask(index)}
+                >
+                  <img src={trashIcon} alt="" />
+                </button>
+              )}
+            </li>
+          ))}
+        </ol>
+        <div className="delete-div">
+          <p>Tiene {tasks.length} tareas pendientes</p>
+          <button className="delete-btn" onClick={deleteAll}>
+            Borrar Todo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ToDoList
+export default ToDoList;
